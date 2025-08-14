bfea8f35bc0e9ab45fda84b309148f978f673dc7
Zabihullah Zabih <zabihullah.buda@gmail.com>
2025-08-14 16:18:27 -0400

    Update Tracking SQL SKILL Progress.sql

--- Tracking SQL SKILL Progress.sql
+++ Tracking SQL SKILL Progress.sql
@@ -45,3 +45,50 @@ SELECT productname, MAX(unitprice)
 FROM products
 GROUP BY productname 
 LIMIT 1;
+
+-- 11: Show all columns from the Suppliers table.
+SELECT * FROM suppliers;
+
+-- 12: Retrieve ProductName and UnitPrice for products in the “Beverages” category.
+SELECT ProductName, UnitPrice
+FROM products p
+JOIN categories c 
+ON p.categoryid = c.categoryid 
+WHERE c.categoryname LIKE '%Beverage%';
+
+-- 23: List OrderID, CustomerID, and ShippedDate for orders shipped by Speedy Express.
+SELECT orderid, customerid, shippeddate
+FROM ORDERs
+WHERE shipname LIKE '%Speedy Express%';
+
+-- 24: List CompanyName and Country for customers in either USA, UK, or Germany.
+SELECT companyname, country
+FROM customers 
+WHERE country IN ('Germany', 'UK', 'Germany');
+
+-- 25: Find all orders with OrderDate between 1996-07-01 and 1996-07-31.
+SELECT * FROM orders 
+WHERE orderdate >= '1996-07-01' AND orderdate <= '1996-07-31';
+
+-- 26: Show ProductName and UnitsInStock for products with more than 50 units in stock ordered by unitsinstock in ascending order.
+SELECT productname, unitsinstock 
+FROM products
+WHERE unitsinstock > 50 
+ORDER BY unitsinstock ASC;
+
+-- 27: For each category, count the number of products in that category.
+SELECT c.categoryname, COUNT(p.ProductID) AS ProductCount 
+FROM categories c
+JOIN products p 
+ON C.categoryid = P.categoryid
+GROUP BY c.categoryname;
+
+-- 28: For each product category, calculate the total sales amount (sum of UnitPrice * Quantity).
+SELECT c.categoryname, SUM(od.unitprice * od.quantity) AS TotalSales
+FROM categories c 
+JOIN products p 
+ON c.categoryid = p.categoryid 
+JOIN order_details od
+ON od.productid = p.productid
+GROUP BY c.categoryname 
+ORDER BY totalsales DESC;
