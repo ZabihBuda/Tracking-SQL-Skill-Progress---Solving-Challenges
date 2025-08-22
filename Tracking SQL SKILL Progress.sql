92aa14d4833497380c4ff290b0979e6af74ee1cb
Zabihullah Zabih <zabihullah.buda@gmail.com>
2025-08-22 14:04:31 -0400

    Update Tracking SQL SKILL Progress.sql

--- Tracking SQL SKILL Progress.sql
+++ Tracking SQL SKILL Progress.sql
@@ -238,3 +238,28 @@ ORDER BY
         ELSE Marks
     END ASC;
 
+
+/* -- 41: Data Lemur Challenge: Write a query to rank the artists within each genre based on their revenue per member and extract the top revenue-generating artist 
+ * from each genre. Display the output of the artist name, genre, concert revenue, number of members, and revenue per band member, sorted by 
+ * the highest revenue per member within each genre.*/
+
+WITH ranked_concerts_cte AS (
+  SELECT 
+    artist_name,
+    genre, 
+    concert_revenue,
+    number_of_members, 
+    (concert_revenue/number_of_members) as revenue_per_member,
+    RANK() OVER (PARTITION BY genre
+    ORDER BY (concert_revenue/number_of_members) DESC) AS ranked_concerts
+  FROM concerts)
+  
+SELECT 
+  artist_name,
+  concert_revenue,
+  genre,
+  number_of_members,
+  revenue_per_member
+FROM ranked_concerts_cte
+WHERE ranked_concerts = 1
+ORDER BY revenue_per_member DESC;
