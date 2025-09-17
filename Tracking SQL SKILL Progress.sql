c4d24d715b35985819f1e882028512e03162cb29
Zabihullah Zabih <zabihullah.buda@gmail.com>
2025-09-17 07:57:43 -0400

    Update Tracking SQL SKILL Progress.sql

--- Tracking SQL SKILL Progress.sql
+++ Tracking SQL SKILL Progress.sql
@@ -593,4 +593,18 @@ WHERE EXTRACT(YEAR FROM dates) = 2021
 GROUP BY user_id
 HAVING COUNT(post_id)>1;
 
+/* 57: Write a query to identify the top 2 Power Users who sent the highest number of messages on Microsoft 
+ * Teams in August 2022. Display the IDs of these 2 users along with the total number of messages they sent.
+ *  Output the results in descending order based on the count of the messages.*/
+
+SELECT 
+  sender_id,
+  count(sender_id) as message_count
+FROM messages
+WHERE EXTRACT(MONTH FROM sent_date) = 08
+  AND EXTRACT(YEAR FROM sent_date) = 2022
+GROUP BY sender_id
+ORDER BY COUNT(sender_id) DESC
+LIMIT 2;
+
 
