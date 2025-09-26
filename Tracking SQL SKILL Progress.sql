a29387cb6565abc95c840b2487e98f3925ddd002
Zabihullah Zabih <zabihullah.buda@gmail.com>
2025-09-26 12:40:03 -0400

    Update Tracking SQL SKILL Progress.sql

--- Tracking SQL SKILL Progress.sql
+++ Tracking SQL SKILL Progress.sql
@@ -647,3 +647,17 @@ WHERE timestamp >= '2022-01-01'
   AND timestamp < '2023-01-01'
 GROUP BY app_id;
 
+/*Assume you're given tables with information about TikTok user sign-ups and confirmations through 
+ * email and text. New users on TikTok sign up using their email addresses, and upon sign-up, each user 
+ * receives a text message confirmation to activate their account.
+
+Write a query to display the user IDs of those who did not confirm their sign-up on the first day, 
+but confirmed on the second day.*/
+
+SELECT 
+  user_id
+FROM emails e 
+INNER JOIN texts t 
+ON e.email_id = t.email_id
+WHERE t.signup_action = 'Confirmed'
+AND t.action_date =  e.signup_date + INTERVAL '1 day';
