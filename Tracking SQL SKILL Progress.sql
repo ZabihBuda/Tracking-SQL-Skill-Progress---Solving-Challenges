d92fd1a74d437e5aef33f008b3b03f362b5adffa
Zabihullah Zabih <zabihullah.buda@gmail.com>
2025-10-17 12:18:50 -0400

    Update Tracking SQL SKILL Progress.sql

--- Tracking SQL SKILL Progress.sql
+++ Tracking SQL SKILL Progress.sql
@@ -790,4 +790,14 @@ FROM user_transaction
 WHERE 
   rn = 3;
 
+/* 67: Imagine you're an HR analyst at a tech company tasked with analyzing employee salaries. Your manager is keen on understanding
+ *  the pay distribution and asks you to determine the second highest salary among all employees.
+
+It's possible that multiple employees may share the same second highest salary. In case of duplicate, display the salary only once.*/
+
+SELECT DISTINCT salary AS second_highest_salary
+FROM employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1;
+
 
