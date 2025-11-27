48d75c5c566bbbdf8930e9f78cf98c764ab30760
Zabihullah Zabih <zabihullah.buda@gmail.com>
2025-11-27 11:03:10 -0500

    Update Tracking SQL SKILL Progress.sql

--- Tracking SQL SKILL Progress.sql
+++ Tracking SQL SKILL Progress.sql
@@ -817,8 +817,8 @@ select
 FROM tweets;
 
 /*69: Assume you're given a table containing data on Amazon customers and their spending on products in different 
- * category, write a query to identify the top two highest-grossing products within each category in the year 2022.
- *  The output should include the category, product, and total spend. */
+ category, write a query to identify the top two highest-grossing products within each category in the year 2022.
+ The output should include the category, product, and total spend. */
 
 
 WITH ranked_spending AS (
@@ -840,4 +840,36 @@ SELECT
   total_spend
 FROM ranked_spending
 WHERE ranking <=2
-ORDER BY category, ranking
+ORDER BY category, ranking;
+
+
+/* 70: As part of an ongoing analysis of salary distribution within the company, your manager has requested a 
+  report identifying high earners in each department. A 'high earner' within a department is defined as an employee 
+ with a salary ranking among the top three salaries within that department.
+
+You're tasked with identifying these high earners across all departments. Write a query to display the employee's 
+name along with their department name and salary. In case of duplicates, sort the results of department name in 
+ascending order, then by salary in descending order. If multiple employees have the same salary, then order them alphabetically.
+
+Note: Ensure to utilize the appropriate ranking window function to handle duplicate salaries effectively.*/
+
+WITH ranked_salary AS (
+  SELECT 
+    name,
+    salary,
+    department_id,
+    DENSE_RANK() OVER (
+      PARTITION BY department_id ORDER BY salary DESC) AS ranking
+  FROM employee
+)
+
+SELECT 
+  d.department_name,
+  s.name,
+  s.salary
+FROM ranked_salary AS s
+INNER JOIN department AS d
+  ON s.department_id = d.department_id
+WHERE s.ranking <= 3
+ORDER BY d.department_name ASC, s.salary DESC, s.name ASC;
+
