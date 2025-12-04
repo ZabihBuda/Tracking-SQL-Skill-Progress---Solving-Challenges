89441780efe9ab7431105329a973a7f3b5d75390
Zabihullah Zabih <zabihullah.buda@gmail.com>
2025-12-04 10:27:58 -0500

    Update Tracking SQL SKILL Progress.sql

--- Tracking SQL SKILL Progress.sql
+++ Tracking SQL SKILL Progress.sql
@@ -922,4 +922,22 @@ GROUP BY
 ORDER BY song_count DESC;
 
 
+/* 73:A Microsoft Azure Supercloud customer is defined as a customer who has purchased at least one product from every product category listed in the products table.
 
+Write a query that identifies the customer IDs of these Supercloud customers.*/
+
+WITH supercloud_cust AS (
+  SELECT 
+    customers.customer_id, 
+    COUNT(DISTINCT products.product_category) AS product_count
+  FROM customer_contracts AS customers
+  INNER JOIN products 
+    ON customers.product_id = products.product_id
+  GROUP BY customers.customer_id
+)
+
+SELECT 
+  customer_id
+FROM supercloud_cust
+WHERE product_count= (
+  SELECT COUNT(DISTINCT product_category) FROM products);
