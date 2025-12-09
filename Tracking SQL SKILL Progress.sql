a51e6909b0360b64cad452fe624d0aac7bec73bb
Zabihullah Zabih <zabihullah.buda@gmail.com>
2025-12-09 10:38:31 -0500

    Update Tracking SQL SKILL Progress.sql

--- Tracking SQL SKILL Progress.sql
+++ Tracking SQL SKILL Progress.sql
@@ -941,3 +941,27 @@ SELECT
 FROM supercloud_cust
 WHERE product_count= (
   SELECT COUNT(DISTINCT product_category) FROM products);
+
+/* 74: Assume you're given a table with measurement values obtained from a Google sensor over multiple days 
+ with measurements taken multiple times within each day.
+
+Write a query to calculate the sum of odd-numbered and even-numbered measurements separately for a particular day 
+and display the results in two different columns. Refer to the Example Output below for the desired format.*/
+
+WITH ranked_measurements AS (
+  SELECT 
+    CAST(measurement_time AS DATE) AS measurement_day, 
+    measurement_value, 
+    ROW_NUMBER() OVER (
+      PARTITION BY CAST(measurement_time AS DATE) 
+      ORDER BY measurement_time) AS measurement_num 
+  FROM measurements
+) 
+
+SELECT 
+  measurement_day,
+  SUM(measurement_value) FILTER (WHERE measurement_num % 2 != 0 ) AS odd_sum,
+  SUM(measurement_value) FILTER (WHERE measurement_num % 2 = 0 ) AS odd_sum
+FROM ranked_measurements
+GROUP BY measurement_day;
+
